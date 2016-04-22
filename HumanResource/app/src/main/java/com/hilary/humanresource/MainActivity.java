5c6979e5457e3e4dc65ddd2ddb6760f52f03c9c4
Hilary <864076727@qq.com>
2016-04-22 22:55:49 +0800

    添加icon

--- HumanResource/app/src/main/java/com/hilary/humanresource/MainActivity.java
+++ HumanResource/app/src/main/java/com/hilary/humanresource/MainActivity.java
@@ -1,27 +1,47 @@
 package com.hilary.humanresource;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TabHost;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView title;
+    private ImageView iv_drawer;
+    private ScrollView drawer;
+    private DrawerLayout drawer_layout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        title=(TextView)findViewById(R.id.title);
+        iv_drawer=(ImageView)findViewById(R.id.iv_drawer);
+        drawer=(ScrollView)findViewById(R.id.drawer);
+        drawer_layout=(DrawerLayout)findViewById(R.id.drawer_layout);
+        iv_drawer.setImageDrawable(getResources().getDrawable(R.drawable.ic_drawer));
+        iv_drawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!drawer_layout.isDrawerOpen(drawer)){
+                    drawer_layout.openDrawer(drawer);
+                }
+            }
+        });
         final TabHost mTabHost = (TabHost) findViewById(android.R.id.tabhost);
-        mTabHost.setup();
+        if(mTabHost!=null)mTabHost.setup();
         final View tab1=getLayoutInflater().inflate(R.layout.tab_button,null);
         TextView tv= (TextView) tab1.findViewById(R.id.tv);
-        TextView title=(TextView)findViewById(R.id.title);
         title.setText("工作");
         tv.setText("工作");
         tv.setTextColor(getResources().getColor(R.color.blue));
@@ -48,7 +68,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTabChanged(String tabId) {
                 if (tabId.equals("tab1")) {
-                    TextView title=(TextView)findViewById(R.id.title);
                     title.setText("工作");
                     TextView tv=(TextView)tab1.findViewById(R.id.tv);
                     tv.setTextColor(getResources().getColor(R.color.blue));
@@ -64,7 +83,6 @@ public class MainActivity extends AppCompatActivity {
                     iv.setImageDrawable(getResources().getDrawable(R.drawable.ic_yingyong2));
                 }
                 if (tabId.equals("tab2")) {
-                    TextView title=(TextView)findViewById(R.id.title);
                     title.setText("联系人");
                     TextView tv=(TextView)tab1.findViewById(R.id.tv);
                     tv.setTextColor(getResources().getColor(R.color.black));
@@ -80,7 +98,6 @@ public class MainActivity extends AppCompatActivity {
                     iv.setImageDrawable(getResources().getDrawable(R.drawable.ic_yingyong2));
                 }
                 if (tabId.equals("tab3")) {
-                    TextView title=(TextView)findViewById(R.id.title);
                     title.setText("应用");
                     TextView tv=(TextView)tab1.findViewById(R.id.tv);
                     tv.setTextColor(getResources().getColor(R.color.black));
